e70a7b44c850f6231f9a0da568abafe4c3ed499d
yuhyeon <klyhyeon@gmail.com>
2025-10-29 12:39:06 +0900

    Initial commit

---
+++ src/main/kotlin/co/yh/agilesoftware/AgileSoftwareApplication.kt
@@ -0,0 +1,11 @@
+package co.yh.agilesoftware
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgileSoftwareApplication
+
+fun main(args: Array<String>) {
+    runApplication<AgileSoftwareApplication>(*args)
+}
---
+++ src/test/kotlin/co/yh/agilesoftware/AgileSoftwareApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.yh.agilesoftware
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgileSoftwareApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
